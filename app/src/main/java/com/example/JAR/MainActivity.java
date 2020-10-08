2f7405585eefa6c8bfb6361423bbdeb2eeda8260
Murray Warne <mw627@kent.ac.uk>
2020-10-08 10:57:45 +0100

    Test Comment

--- app/src/main/java/com/example/JAR/MainActivity.java
+++ app/src/main/java/com/example/JAR/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 // Emilio comment
 // Magd comment
+// Murray Comment
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
