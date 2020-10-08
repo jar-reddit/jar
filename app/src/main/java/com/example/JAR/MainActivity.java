b06c71f9d90ded7a9ec78efc5e562cec2fb66459
Magd Gabr <mmmg3@kent.ac.uk>
2020-10-08 10:54:50 +0100

    test comment

--- app/src/main/java/com/example/JAR/MainActivity.java
+++ app/src/main/java/com/example/JAR/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.JAR;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 // Emilio comment
+// Magd comment
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
