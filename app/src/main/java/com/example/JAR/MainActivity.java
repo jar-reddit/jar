0030e5981a1447ca4176f2fdfa546c953d0e3516
A.Mueed <am2398@kent.ac.uk>
2020-10-13 09:00:32 +0100

    fixed bug where only 2 posts were shown

--- app/src/main/java/com/example/JAR/MainActivity.java
+++ app/src/main/java/com/example/JAR/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.JAR;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 import android.widget.TextView;
 
@@ -22,6 +23,7 @@ import java.util.UUID;
 public class MainActivity extends AppCompatActivity {
     public String testText; // TODO: remove these public variables
     public TextView testView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,45 +31,35 @@ public class MainActivity extends AppCompatActivity {
         AndroidNetworking.initialize(getApplicationContext());
 
         testView = findViewById(R.id.test_text);
-        AndroidNetworking.get("https://www.reddit.com/r/frontpage/.json")
+        //TODO: make our own Reddit API Wrapper
+        AndroidNetworking.get("https://www.reddit.com/.json")
                 .setUserAgent("android:com.example.JAR:v0.0.1 (by /u/mueea001)")
-                .build().getAsJSONObject(new JSONObjectRequestListener() {
-            @Override
-            public void onResponse(JSONObject response) {
-                TextView out = findViewById(R.id.test_text);
-                try {
-                    String outText =  "length: " + response.length() + "\n";
-                    for(int i = 0; i < response.length(); i++) {
-                        outText += "title: " + response.getJSONObject("data").getJSONArray("children").getJSONObject(i).getJSONObject("data").getString("title");
-                        outText += "\nselftext: " + response.getJSONObject("data").getJSONArray("children").getJSONObject(i).getJSONObject("data").getString("selftext") + "\n";
+                .build()
+                .getAsJSONObject(new JSONObjectRequestListener() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        TextView out = findViewById(R.id.test_text);
+                        try {
+
+                            int length = response.getJSONObject("data").getInt("dist");
+                            String outText = "length: " + length + "\n";
+                            for (int i = 0; i < length; i++) {
+                                outText += "\nsub: " + response.getJSONObject("data").getJSONArray("children").getJSONObject(i).getJSONObject("data").getString("subreddit");
+                                outText += "\ntitle: " + response.getJSONObject("data").getJSONArray("children").getJSONObject(i).getJSONObject("data").getString("title");
+                                outText += "\nselftext: " + response.getJSONObject("data").getJSONArray("children").getJSONObject(i).getJSONObject("data").getString("selftext") + "\n";
+                            }
+                            out.setText(outText);
+                        } catch (JSONException e) {
+                            out.setText("JSON error");
+                            e.printStackTrace();
+                        }
                     }
-                    out.setText(outText);
-                } catch (JSONException e) {
-                    out.setText("JSON error");
-                    e.printStackTrace();
-                }
-            }
 
-            @Override
-            public void onError(ANError anError) {
+                    @Override
+                    public void onError(ANError anError) {
 
-            }
-        })
-//                .getAsString(new StringRequestListener() {
-//                    @Override
-//                    public void onResponse(String response) {
-//                        TextView out = findViewById(R.id.test_text);
-//                        out.setText(response);
-//                    }
-//
-//                    @Override
-//                    public void onError(ANError anError) {
-//                        testText = anError.toString();
-//                    }
-//                }
-//                )
-        ;
-
+                    }
+                });
 
 
         testView.setText("please wait");
