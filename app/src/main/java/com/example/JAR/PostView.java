32aaca91f559ed0255ee04afb0aef5b7037189a5
Murray Warne <mw627@kent.ac.uk>
2020-11-19 08:36:38 +0000

    Commented out the code until i can fully understand it and make it compilable

--- app/src/main/java/com/example/JAR/PostView.java
+++ app/src/main/java/com/example/JAR/PostView.java
@@ -19,7 +19,9 @@ import androidx.annotation.Nullable;
 
 import com.example.JAR.databinding.ViewPostBinding;
 
+import net.dean.jraw.models.PublicContribution;
 import net.dean.jraw.models.Submission;
+import net.dean.jraw.tree.RootCommentNode;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -111,12 +113,18 @@ public class PostView extends LinearLayout {
 
     }
 
-    public void getComments()
+    /*public void getComments()
             // https://mattbdean.gitbooks.io/jraw/content/cookbook.html
     {
-        
+        RootCommentNode comment = redditClient.submission("need to find a way to pull the post id");
+        // This line is used to retrieve the comments from the post
+        Iterator<CommentNode<PublicContribution<?>>> it = root.walkTree().iterator();
 
-    }
+        while (it.hasNext()) {
+            PublicContribution<?> thing = it.next().getSubject();
+            System.out.println(thing.getBody());
+        }
+    }*/
 
 
 }
