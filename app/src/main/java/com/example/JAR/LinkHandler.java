9e84ccfa6b96f531d742672eefda4dba1998adea
Abdul Mueed <am2398@kent.ac.uk>
2020-12-08 10:08:33 +0000

    LinkHandler can open submissions now

--- app/src/main/java/com/example/JAR/LinkHandler.java
+++ app/src/main/java/com/example/JAR/LinkHandler.java
@@ -1,10 +1,13 @@
 package com.example.JAR;
 
+import android.app.Activity;
 import android.app.SearchManager;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.util.Log;
+
+import net.dean.jraw.models.Submission;
 
 import java.util.List;
 
@@ -15,10 +18,24 @@ public class LinkHandler {
         if (uriPath.get(0).equals("r")) {
             Log.d("JAR URL",uriPath.get(1));
             subreddit = uriPath.get(1);
-            Intent subredditIntent = new Intent(context, SubredditActivity.class);
-            subredditIntent.setAction(Intent.ACTION_SEARCH);
-            subredditIntent.putExtra("query", subreddit);
-            context.startActivity(subredditIntent);
+            if (uriPath.size() >=4 && uriPath.get(2).equals("comments")) {
+                Intent submissionIntent = new Intent(context, SubmissionActivity.class);
+                Background.execute(() -> {
+                    Log.d("Post URL",uriPath.get(3));
+                    Submission s = JRAW.getInstance(context).submission(uriPath.get(3)).inspect();
+                    ((Activity) context).runOnUiThread(()-> {
+                        submissionIntent.putExtra("Post",s);
+                        context.startActivity(submissionIntent);
+                    });
+                });
+
+
+            } else {
+                Intent subredditIntent = new Intent(context, SubredditActivity.class);
+                subredditIntent.setAction(Intent.ACTION_SEARCH);
+                subredditIntent.putExtra("query", subreddit);
+                context.startActivity(subredditIntent);
+            }
         } else {
             Intent subredditIntent = new Intent(context, SubredditActivity.class);
             context.startActivity(subredditIntent);
