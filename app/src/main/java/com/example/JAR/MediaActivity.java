2cfb97f22054f571f87a7c7b47fee16ae9269aef
Abdul Mueed <am2398@kent.ac.uk>
2020-12-15 09:04:24 +0000

    Improved image viewer

--- app/src/main/java/com/example/JAR/MediaActivity.java
+++ app/src/main/java/com/example/JAR/MediaActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
 
 import com.bumptech.glide.Glide;
 import com.example.JAR.databinding.ViewImageBinding;
@@ -11,7 +12,7 @@ import com.example.JAR.databinding.ViewMediaBinding;
 
 import net.dean.jraw.models.Submission;
 
-public class MediaActivity extends AppCompatActivity {
+public class MediaActivity extends FragmentActivity {
     public ViewImageBinding binding;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -19,6 +20,12 @@ public class MediaActivity extends AppCompatActivity {
         binding = ViewImageBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
         Submission submission = (Submission) getIntent().getExtras().get("Post");
-        Glide.with(this).load(submission.getUrl()).into(binding.subImage);
+        Glide
+                .with(this)
+                .load(submission.getUrl())
+                .error(Glide.with(this).load(submission.getPreview().getImages().get(0).getSource().getUrl()))
+                .into(binding.subImage)
+                ;
+
     }
 }
