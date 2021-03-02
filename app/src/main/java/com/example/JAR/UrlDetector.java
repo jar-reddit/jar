8bd71f88d7cd3d47d39e7e761ad18fcbb01dfca6
Abdul Mueed <am2398@kent.ac.uk>
2021-03-02 11:42:35 +0000

    Url detector works on i.redd.it and v.redd.it

---
+++ app/src/main/java/com/example/JAR/UrlDetector.java
@@ -0,0 +1,44 @@
+package com.example.JAR;
+
+import android.util.Log;
+
+import net.dean.jraw.models.Sorting;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+/**
+ * The {@link UrlDetector} class is used to handle the detection of URLs
+ */
+public class UrlDetector {
+
+    /**
+     * This method will try to detect the type of the given url
+     * @param url a url
+     * @return the type of the url
+     */
+    public static String detect(URL url) {
+        Log.d("Detect URL", url.toString());
+        switch (url.getAuthority()) {
+            case "i.redd.it":
+                return "reddit:image";
+            case "v.redd.it":
+                return "reddit:video";
+            case "i.imgur.com":
+                Log.d("Imgur",url.getPath());
+                if (url.getPath().endsWith(".gifv")){
+                    return "imgur:video";
+                }
+        }
+        return "unknown";
+    }
+    
+    public static String detect(String url) {
+        try {
+            return detect(new URL(url));
+        } catch (MalformedURLException e) {
+            Log.d("JAR/URL",e.getMessage());
+        }
+        return "bad url";
+    }
+}
