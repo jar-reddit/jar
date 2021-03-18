5100e1d08c38ab692ad6303e17b1e694b935fce7
Abdul Mueed <am2398@kent.ac.uk>
2021-03-18 23:23:18 +0000

    Sorted out recycler view or wasted a lot of time moving code. voting works!!!!!!!!!

--- app/src/main/java/com/example/JAR/PostViewHolder.java
+++ app/src/main/java/com/example/JAR/PostViewHolder.java
@@ -1,6 +1,8 @@
 package com.example.JAR;
 
 import android.content.Context;
+import android.graphics.Color;
+import android.text.Html;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,24 +12,162 @@ import android.widget.LinearLayout;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
+import com.example.JAR.databinding.ViewPostBinding;
+import com.x5.template.Chunk;
+
 import net.dean.jraw.models.Submission;
+import net.dean.jraw.models.SubmissionPreview;
+import net.dean.jraw.models.VoteDirection;
+import net.dean.jraw.oauth.AuthManager;
+
+import me.saket.bettermovementmethod.BetterLinkMovementMethod;
 
 
 public class PostViewHolder extends RecyclerView.ViewHolder {
     private final PostView postItemView;
+    private ViewPostBinding viewPostBinding;
+    private VoteDirection vote;
+    private Submission post;
+
     public PostViewHolder(@NonNull View itemView) {
         super(itemView);
         postItemView = (PostView) itemView;
+        viewPostBinding = ViewPostBinding.bind(postItemView);
     }
 
     public void bind(Submission post) {
-        postItemView.setPost(post);
-        Log.d("JAR:PostViewHolder", "set the post for " +post.getId());
+        setUnvoteColour();
+        this.post = post;
+        String previewUrl = "";
+        if (post.hasThumbnail()) {
+            // fallback image
+            previewUrl = post.getThumbnail();
+        }
+        int largestWidth = 1080; // could be set from settings
+        if (post.getPreview() != null/* && post.getPreview().isEnabled()*/) {
+            SubmissionPreview.ImageSet img = post.getPreview().getImages().get(0);
+            for (SubmissionPreview.Variation variation : img.getResolutions()) {
+                if (variation.getWidth() > largestWidth) {
+                    break;
+                }
+                previewUrl = variation.getUrl();
+            }
+        }
+        Glide.with(viewPostBinding.getRoot()).load(previewUrl).into(viewPostBinding.postImage);
+        viewPostBinding.getRoot().setOnClickListener((view)->{
+            NavigationHandler.openSubmission(post, view.getContext());
+        });
+
+        viewPostBinding.postImage.setOnClickListener(view -> NavigationHandler.openLink(view.getContext(), post));
+
+        BetterLinkMovementMethod method = BetterLinkMovementMethod.linkifyHtml(viewPostBinding.otherInfo);
+        method.setOnLinkClickListener((textView, url) -> {
+            Log.d("Clicked link", url);
+            NavigationHandler.openLink(textView.getContext(), url);
+            return true;
+        });
+        // TODO: 18/03/2021 Handle voting when user is offline
+        viewPostBinding.postUpvoteButton.setOnClickListener((view) -> {
+            upvote();
+            vote(vote);
+        });
+        viewPostBinding.postDownvoteButton.setOnClickListener((view) -> {
+            downvote();
+            vote(vote);
+        });
+        vote = post.getVote();
+        setVoteColour();
+        viewPostBinding.textScore.setText(String.valueOf(post.getScore()));
+        viewPostBinding.textComments.setText(String.valueOf(post.getCommentCount()));
+        String format = Settings.getSettings(postItemView.getContext()).getString("posts.format");
+        Chunk chunk = new Chunk();
+        chunk.append(format);
+        chunk.set("subreddit", post.getSubreddit());
+        chunk.set("author", post.getAuthor());
+        chunk.set("self_text", post.getSelfText());
+        chunk.set("self_text_html", post.getSelfTextHtml());
+        chunk.set("time", post.getCreated());
+        chunk.set("flair", post.getLinkFlairText());
+        chunk.set("title", post.getTitle());
+        chunk.setMultiple(Settings.getSettings(postItemView.getContext()).toMap());
+
+        viewPostBinding.otherInfo.setText(Html.fromHtml(chunk.toString(), Html.FROM_HTML_SEPARATOR_LINE_BREAK_PARAGRAPH));
+        chunk.resetTemplate();
+        chunk.append(Settings.getSettings(postItemView.getContext()).getString("posts.title"));
+        viewPostBinding.postTitle.setText(Html.fromHtml(chunk.toString(), Html.FROM_HTML_SEPARATOR_LINE_BREAK_PARAGRAPH));
     }
 
     public static PostViewHolder create(ViewGroup parent) {
         PostView view = new PostView(parent.getContext());
         view.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
         return new PostViewHolder(view);
+    }
+
+    private void upvote() {
+        if (vote.equals(VoteDirection.UP)) {
+            setUnvoteColour();
+            vote = VoteDirection.NONE;
+        } else {
+            setUpvoteColour();
+            vote = VoteDirection.UP;
+        }
+    }
+
+    private void downvote() {
+        if (vote.equals(VoteDirection.DOWN)) {
+            setUnvoteColour();
+            vote = VoteDirection.NONE;
+        } else {
+            setDownvoteColour();
+            vote = VoteDirection.DOWN;
+        }
+    }
+
+    private void setUpvoteColour() {
+
+        viewPostBinding.postUpvoteButton.setColorFilter(Color.parseColor(Settings.getSettings(postItemView.getContext()).getString("general.upvote")));
+        viewPostBinding.postDownvoteButton.setColorFilter(0xFFFFFF);
+    }
+
+    private void setDownvoteColour() {
+        viewPostBinding.postDownvoteButton.setColorFilter(Color.parseColor(Settings.getSettings(postItemView.getContext()).getString("general.downvote")));
+        viewPostBinding.postUpvoteButton.setColorFilter(0xFFFFFF);
+    }
+
+    private void setUnvoteColour() {
+        viewPostBinding.postUpvoteButton.setColorFilter(0xFFFFFF);
+        viewPostBinding.postDownvoteButton.setColorFilter(0xFFFFFF);
+    }
+
+    private void hideVoting() {
+        viewPostBinding.postUpvoteButton.setVisibility(View.INVISIBLE);
+        viewPostBinding.postDownvoteButton.setVisibility(View.INVISIBLE);
+    }
+
+    private void setVoteColour() {
+        if (!JRAW.getInstance(postItemView.getContext()).getAuthManager().currentUsername().equals(AuthManager.USERNAME_USERLESS)) {
+            if (vote.equals(VoteDirection.UP)) {
+                setUpvoteColour();
+            } else if (vote.equals(VoteDirection.DOWN)) {
+                setDownvoteColour();
+            }
+        } else {
+            hideVoting();
+        }
+    }
+
+    private void vote(VoteDirection direction) {
+        Background.execute(()->{
+            post.toReference(JRAW.getInstance(postItemView.getContext())).setVote(direction);
+        });
+    }
+
+    private void getVote() {
+//        Background.execute(()->{
+//            vote = post.toReference(JRAW.getInstance(postItemView.getContext())).inspect().getVote();
+//            setVoteColour();
+//        });
+
     }
 }
