ea2b4dd5fa1c293f42308c3882601ed84ebe25eb
E <ee274@kent.ac.uk>
2021-04-07 11:35:43 +0100

    Fixed image downloading on SubmissionActivity

--- app/src/main/java/com/example/JAR/SubmissionActivity.java
+++ app/src/main/java/com/example/JAR/SubmissionActivity.java
@@ -1,10 +1,18 @@
 package com.example.JAR;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.os.Environment;
 import android.text.Html;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,15 +22,19 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.app.ActivityCompat;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.DataSource;
 import com.bumptech.glide.load.engine.GlideException;
 import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.target.CustomTarget;
 import com.bumptech.glide.request.target.Target;
+import com.bumptech.glide.request.transition.Transition;
 import com.example.JAR.databinding.ActivitySubmissionBinding;
 import com.example.JAR.databinding.ViewCommentBinding;
 
@@ -34,6 +46,10 @@ import net.dean.jraw.references.PublicContributionReference;
 import net.dean.jraw.tree.CommentNode;
 import net.dean.jraw.tree.RootCommentNode;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+
 import me.saket.bettermovementmethod.BetterLinkMovementMethod;
 
 
@@ -44,11 +60,11 @@ public class SubmissionActivity extends AppCompatActivity {
     private ImageView image;
     private ImageButton upVote;
     private ImageButton downVote;
-    private ImageButton share;
     private Drawable thumbnail;
     private TextView score;
     private TextView commentScore;
     private ActivitySubmissionBinding views;
+    private boolean hideDownload = true;
 
 
     public void onCreate(Bundle savedInstanceState) {
@@ -59,7 +75,31 @@ public class SubmissionActivity extends AppCompatActivity {
         getComments();
         setContent();
         isUserless();
+        isImagePost();
 
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_submission, menu);
+        if(hideDownload) {
+            MenuItem item = menu.findItem(R.id.download);
+            item.setVisible(false);
+        }
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.share:
+                shareContent(post.getUrl());
+                return true;
+            case R.id.download:
+                downloadImage(post.getUrl());
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
     private void getPost() {
@@ -111,21 +151,11 @@ public class SubmissionActivity extends AppCompatActivity {
         views.submissionUser.setText(post.getAuthor());
         upVote = (ImageButton) findViewById(R.id.Upvote);
         downVote = (ImageButton) findViewById(R.id.downvote);
-        share = (ImageButton) findViewById(R.id.shareBtn);
-
-        share.setOnClickListener(new View.OnClickListener()
-        {
-            @Override
-            public void onClick(View v)
-            {
-                shareContent(post.getUrl());
-            }
-        });
 
         upVote.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-//                upVoting(post);
+                upVoting(post);
                 Log.d("Userless", String.valueOf(isUserless()));
             }
         });
@@ -133,7 +163,7 @@ public class SubmissionActivity extends AppCompatActivity {
         downVote.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-//                downVoting(post);
+                downVoting(post);
                 Log.d("Userless", String.valueOf(isUserless()));
             }
         });
@@ -289,8 +319,85 @@ public class SubmissionActivity extends AppCompatActivity {
         startActivity(shareIntent);
     }
 
-//    public String currentUsername() {
-//        return App.getAccountHelper().getReddit().getAuthManager().currentUsername();
-//    }
+    public void downloadImage(String URL){
 
+        if (!checkStoragePermissions()) {
+            return;
+        }
+
+        String dirPath = Environment.getExternalStorageDirectory().getAbsolutePath() + "/" + getString(R.string.app_name) + "/";
+
+        final File directory = new File(dirPath);
+
+        final String fileName = URL.substring(URL.lastIndexOf('/') + 1);
+
+        Glide.with(this)
+                .load(URL)
+                .into(new CustomTarget<Drawable>() {
+                    @Override
+                    public void onResourceReady(@NonNull Drawable resource, @Nullable Transition<? super Drawable> transition) {
+
+                        Bitmap bitmap = ((BitmapDrawable)resource).getBitmap();
+                        Toast.makeText(SubmissionActivity.this, "Saving image to storage", Toast.LENGTH_SHORT).show();
+                        saveImage(bitmap, directory, fileName);
+                    }
+
+                    @Override
+                    public void onLoadCleared(@Nullable Drawable placeholder) {
+
+                    }
+
+                    @Override
+                    public void onLoadFailed(@Nullable Drawable placeholder) {
+                        super.onLoadFailed(placeholder);
+
+                        Toast.makeText(SubmissionActivity.this, "Image download failed", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+
+    }
+
+    public Boolean checkStoragePermissions() {
+
+        // This will return the current Status
+        int externalStoragePerms = ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+
+        if (externalStoragePerms != PackageManager.PERMISSION_GRANTED) {
+
+            String[] STORAGE_PERMISSIONS = {Manifest.permission.WRITE_EXTERNAL_STORAGE};
+            // If permission not granted then ask for permission real time.
+            ActivityCompat.requestPermissions(this, STORAGE_PERMISSIONS, 1);
+            return false;
+        }
+
+        return true;
+    }
+
+    private void saveImage(Bitmap image, File imageDirectory, String imageName) {
+
+
+        if (!imageDirectory.exists()) {
+            imageDirectory.mkdir();
+        }
+            File imageFile = new File(imageDirectory, imageName);
+            try {
+                OutputStream fOut = new FileOutputStream(imageFile);
+                image.compress(Bitmap.CompressFormat.JPEG, 100, fOut);
+                fOut.close();
+                Toast.makeText(SubmissionActivity.this, "Image saved to storage", Toast.LENGTH_SHORT).show();
+            } catch (Exception e) {
+                Toast.makeText(SubmissionActivity.this, "Error during save", Toast.LENGTH_SHORT).show();
+                e.printStackTrace();
+            }
+    }
+
+    public void isImagePost()
+    {
+       if(UrlDetector.detect(post.getUrl()).contains("image"))
+       {
+            hideDownload = false;
+       }
+
+    }
 }
